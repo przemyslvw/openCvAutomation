dd2478828bd6616e2f1c4e4297d4a9630c0ad0ef
przemyslvw <przemyslvw@gmial.com>
2024-08-30 22:46:54 +0200

    fix

--- main.py
+++ main.py
@@ -1,17 +1,44 @@
-from hackrf import HackRF
-from pylab import psd, xlabel, ylabel, show
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.io import wavfile
+from scipy.fft import fft, fftfreq
 
-# Initialize HackRF device
-with HackRF() as hrf:
-    # Set sample rate and center frequency
-    hrf.sample_rate = 20e6
-    hrf.center_freq = 88.5e6
+# Ustawienia odbioru
+frequency = 915e6  # Częstotliwość odbioru (915 MHz)
+sample_rate = 10e6  # Prędkość próbkowania (10 MS/s)
+duration = 5  # Czas trwania w sekundach
 
-    # Read samples from HackRF
-    samples = hrf.read_samples(2e6)
+# Komenda do odbioru sygnału
+os.system(f'hackrf_transfer -r captured_data.bin -f {frequency} -s {sample_rate} -t {duration}')
 
-    # Use matplotlib to estimate and plot the PSD
-    psd(samples, NFFT=1024, Fs=hrf.sample_rate/1e6, Fc=hrf.center_freq/1e6)
-    xlabel('Frequency (MHz)')
-    ylabel('Relative power (dB)')
-    show()
+# Odczytaj dane z pliku binarnego
+def read_binary_file(file_path):
+    data = np.fromfile(file_path, dtype=np.complex64)
+    return data
+
+data = read_binary_file('captured_data.bin')
+
+# Analiza sygnału
+N = len(data)
+yf = fft(data)
+xf = fftfreq(N, 1 / sample_rate)
+
+# Wizualizacja
+plt.figure(figsize=(10, 6))
+
+plt.subplot(2, 1, 1)
+plt.title('Odczytane dane')
+plt.plot(np.real(data))
+plt.plot(np.imag(data))
+plt.xlabel('Próbki')
+plt.ylabel('Amplituda')
+
+plt.subplot(2, 1, 2)
+plt.title('Spektrum częstotliwości')
+plt.plot(xf, np.abs(yf))
+plt.xlabel('Częstotliwość (Hz)')
+plt.ylabel('Amplituda')
+
+plt.tight_layout()
+plt.show()
